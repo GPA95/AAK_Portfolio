8372159e2145955724aa94f4aad7b0c79e8f3648
Ammaar Ahmad Khan <ammaarahmadkhan16@gmail.com>
2025-06-28 19:04:21 +0530

    Updated Headline to CS Junior

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -16,17 +16,15 @@ function Intro() {
             Hey, I'm <span className="highlight">Ammaar Ahmad Khan</span>!
           </h1>
           <p className="headline">
-            CS Sophomore | Data Science & AI Enthusiast | Aspiring Engineer
+            CS Junior | AI & Data Science Enthusiast | Aspiring Engineer
           </p>
           <p className="bio">
-          As a 2nd-year Computer Science and Engineering student at Integral University, Lucknow, I am passionate about solving real-world problems through AI, Data Science, and Software Engineering. My academic foundation includes Python, SQL, Java, and C, along with growing expertise in Web Development and Robotics Software.
+          As a 3rd-year Computer Science and Engineering student at Integral University, Lucknow, I am passionate about solving real-world problems through AI, Data Science, and Software Engineering. My academic foundation includes Python, SQL, Java, and C, along with growing expertise in Web Development and Robotics Software.
           </p>
           <div className="cta-buttons">
           <a href="https://drive.google.com/file/d/1M6ItSGVehXU9Lx2JcuopREkY6nrHHf2h/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-outline">📄 View My CV</a>
 
-            <a href="https://www.linkedin.com/in/ammaar-ahmad-khan-0044b9320/" target="_blank" className="btn btn-outline">
-              🔗 View LinkedIn
-            </a>
+          <a href="https://www.linkedin.com/in/ammaar-ahmad-khan-0044b9320/" target="_blank" className="btn btn-outline">🔗 View LinkedIn</a>
           </div>
         </div>
       </div>
