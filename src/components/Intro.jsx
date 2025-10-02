e3dfb2434f18eb4541913b41e463657ec9105c46
Ammaar Ahmad Khan <ammaarahmadkhan16@gmail.com>
2025-10-02 15:49:57 +0530

    Replaced CV with Resume

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -22,7 +22,7 @@ function Intro() {
           I’m a third-year Computer Science student, passionate about solving real-world problems through Artificial Intelligence, Data Science, and Software Engineering. My experience spans across AI/ML, full-stack web development, data visualization, and robotics software. I’ve built projects ranging from responsive web apps and business dashboards to research-driven robotics. With certifications and hands-on hackathon experience, I actively pursue opportunities to apply my skills through impactful projects and internships.
           </p>
           <div className="cta-buttons">
-          <a href="https://drive.google.com/file/d/1M6ItSGVehXU9Lx2JcuopREkY6nrHHf2h/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-outline">📄 View My CV</a>
+          <a href="https://drive.google.com/file/d/1xXfnU3xjMk_IAhP-M9iInxPQQvgBe-Xk/view?usp=sharing" target="_blank" rel="noopener noreferrer" className="btn btn-outline">📄 View My Resume</a>
 
           <a href="https://www.linkedin.com/in/ammaar-ahmad-khan/" target="_blank" className="btn btn-outline">🔗 View LinkedIn</a>
           </div>
