0f825ad3a1c918114ffa327e8562137ad2e9a9d5
Ammaar Ahmad Khan <ammaarahmadkhan16@gmail.com>
2026-02-17 01:57:37 +0530

    updated clg details for 5th sem

--- src/components/CollegeCard.jsx
+++ src/components/CollegeCard.jsx
@@ -12,7 +12,7 @@ export default function CollegeCard() {
         <div className="edu-right">
           <h3>Integral University, Lucknow, Uttar Pradesh, India</h3>
           <p className="edu-subtitle">B.Tech – Computer Science and Engineering (2023–2027)</p>
-          <p className="edu-subtitle">Ongoing 5th Semester</p>
+          <p className="edu-subtitle">Ongoing 6th Semester</p>
         </div>
       </div>
 
